08e712f6551766129e5f5b2b0d35cc4a6545d953
linjh <516308181@qq.com>
2023-06-13 10:48:06 +0800

    FIX

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
+import { CacheModule ,CacheModuleOptions} from '@nestjs/cache-manager';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from '@/utils';
+import { redisStore } from 'cache-manager-redis-yet';
+
+const RedisCacheOptions: CacheModuleOptions = {
+  isGlobal: true,
+  store: redisStore,
+  host: getConfig("REDIS_CONFIG").host,
+  port: getConfig("REDIS_CONFIG").port,
+  auth_pass: getConfig("REDIS_CONFIG").auth_pass,
+  db: getConfig("REDIS_CONFIG").db,
+};
 @Module({
   imports: [
-    CacheModule.register({
-      isGlobal: true,
-    }),
+    CacheModule.register(RedisCacheOptions),
     ConfigModule.forRoot({
       ignoreEnvFile: true,
       isGlobal: true,
